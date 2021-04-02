36db007b1246e5cfd3907d80c0087d5b8ddeab19
Edward <24864616+samklop@users.noreply.github.com>
2021-04-02 13:46:06 +0200

    Fixes for production environment

--- api/index.php
+++ api/index.php
@@ -10,17 +10,11 @@
     $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     $uri = explode('/', $uri);
 
-    if(!isset($uri[1]) || $uri[1] !== 'api')
-    {
-        header("HTTP/1.1 404 Not Found");
-        exit();
-    }
-
     $requestMethod = $_SERVER["REQUEST_METHOD"];
     $function = null;
-    if (isset($uri[2]))
+    if (isset($uri[1]))
     {
-        $function = $uri[2];
+        $function = $uri[1];
     }
 
     $controller = new Post($requestMethod, $function);
